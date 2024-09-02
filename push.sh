680703604e569c38b44baaec46c34930bbfb3d02
Hanson-zhy <github-hanson@outlook.com>
2024-09-02 21:55:54 +0800

    update

--- push.sh
+++
@@ -1,8 +0,0 @@
-rm -rf *
-cp -r /sd/Website/homepage/s* .
-cp -r /sd/Website/homepage/i* .
-cp -r /sd/Website/homepage/r* .
-git add .
-git commit -m "update"
-git pull --rebase origin main
-git push -u origin main
